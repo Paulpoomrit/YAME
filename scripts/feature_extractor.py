b7edca06e22ab2213bfe7e01fbc03d6ef5cd5287
Paulpoomrit <paulpoomrit@gmail.com>
2026-04-03 10:52:41 -0700

    Added rule of three ratio function

--- scripts/feature_extractor.py
+++ scripts/feature_extractor.py
@@ -3,6 +3,8 @@ import re
 import language_tool_python
 import nltk
 nltk.download('punkt_tab')
+nltk.download('averaged_perceptron_tagger_eng')
+nltk.download('universal_tagset')
 
 class Feature(Enum):
     # --------- Group A: Features related to language, grammar and style --------- #
@@ -66,7 +68,8 @@ def count_all_grammartical_mistakes(text: str) -> int:
         return len(matches)
     return -1
 
-def count_negative_parallelism(text: str) -> int:
+
+def negative_parallelism_ratio(text: str) -> int:
     """ Returns the ratio of sentences with negative parallelism to
     all sentences with negative sentiment
     (that does or does not come with its parallel counterpart). """
@@ -79,6 +82,34 @@ def count_negative_parallelism(text: str) -> int:
         return 0
 
     return len(neg_paralleism_match)/ len(all_neg_match)
+
+
+def rule_of_three_ratio(text: str) -> int:
+    total_num_senteces = 0
+    total_rule_of_three_sentences = 0
+    sentences = nltk.sent_tokenize(text)
+    rule_of_three_regex = re.compile(r"\b(\w+)\b,\s(.+),\s(.+)")
+
+    for sentence in sentences:
+        total_num_senteces += 1
+        # print(sentence)
+        match = rule_of_three_regex.findall(sentence)
+
+
+        if len(match) > 0:
+            match_list = list(match[0])
+            tagged = nltk.pos_tag(match_list, tagset='universal')
+            # print(tagged)
+
+            # Two of the examples has to be of the same type to qualify
+            if (tagged[0][1] == tagged[1][1] or tagged[1][1] == tagged[2][1] or tagged[0][1] == tagged[2][1]):
+                total_rule_of_three_sentences += 1
+
+    if total_num_senteces == 0 or total_rule_of_three_sentences == 0:
+        return 0
+
+    return total_rule_of_three_sentences/total_num_senteces
+
 
 def extract_group_a(text: str, feature_type: Feature, total_num_words: int):
 
@@ -100,5 +131,7 @@ def extract_features(document_path: str):
 ## Test
 #extract_features("/Users/paulpoomrit/1_SFU/8_Spring_2026/LING450_CompLing/LING450_TermProject/data/coca-samples-text/text_acad.txt")
 #print(count_all_grammartical_mistakes("I is Paul."))
-print(count_negative_parallelism("We live in capitalism, its power seems inescapable — but then, so did the divine right of kings. Any human power can be resisted and changed by human beings. Resistance and change often begin in art. Very often in our art, the art of words."))
-print(count_negative_parallelism("a Not just x but y. ain't x rather k. That’s not just a sourcing issue—it's a systemic bias. The issue here isn't just sourcing—it's framing. The issue here is not just sourcing—it's framing. Constitutes not only a work of self-representation, but a visual document. huh"))
+# print(negative_parallelism_ratio("We live in capitalism, its power seems inescapable — but then, so did the divine right of kings. Any human power can be resisted and changed by human beings. Resistance and change often begin in art. Very often in our art, the art of words."))
+# print(negative_parallelism_ratio("a Not just x but y. ain't x rather k. That’s not just a sourcing issue—it's a systemic bias. The issue here isn't just sourcing—it's framing. The issue here is not just sourcing—it's framing. Constitutes not only a work of self-representation, but a visual document. huh"))
+
+print(rule_of_three_ratio("This is so insane, right brother? The Amaze Conference brings together global SEO professionals, marketing experts, and growth hackers to discuss the latest trends in digital marketing. The event features keynote sessions, panel discussions, and networking opportunities."))
